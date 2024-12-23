f5bba1686a502f38b8b3d0719b2cd2f7c70d2aa3
andreileonte1981 <adw_ro@yahoo.co.uk>
2024-12-23 14:02:19 +0200

    Logs in

--- dashboard/app/routes/home.tsx
+++ dashboard/app/routes/home.tsx
@@ -9,9 +9,11 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader({}) {
+export async function clientLoader({}) {
   // TODO: check logged in; wrap all routes except login and register in a component that redirects to login
-  return redirect("login");
+  if (!localStorage.getItem("jwt")) {
+    return redirect("login");
+  }
 }
 
 export default function Home() {
--- dashboard/app/routes/login.tsx
+++ dashboard/app/routes/login.tsx
@@ -1,18 +1,51 @@
-import { Link } from "react-router";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { ModalContext } from "~/context/modalContext";
 
 export default function Login() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const { isOpen, setIsOpen, message, setMessage } = useContext(ModalContext);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const url = "http://127.0.0.1:5000/api/users/login";
+      const response = await axios.post(url, formData);
+
+      if (response.status === 200) {
+        localStorage.setItem("jwt", response.data.token);
+
+        navigate("..");
+      }
+    } catch (error: any) {
+      debugger;
+
+      setMessage(error.response?.data?.message || "Login error");
+      setIsOpen(true);
+    }
+  };
+
+  const handleChange = (event: any) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
   return (
     <>
-      <form>
-        <label htmlFor="Email">
-          <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          <input type="text" name="email" onChange={handleChange} />
         </label>
-        <label htmlFor="Password">
-          <input type="password" />
+        <label htmlFor="password">
+          <input type="password" name="password" onChange={handleChange} />
         </label>
         <button type="submit">Log in</button>
       </form>
       <Link to="/register">Register</Link>
-      </>
+    </>
   );
 }
