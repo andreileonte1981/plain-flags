1c5d5c03340a7ede76e81ffd23a68f393e22a8f0
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-01-22 16:33:23 +0200

    Flag details page populated with some flag data

--- dashboard/app/routes/flag.tsx
+++ dashboard/app/routes/flag.tsx
@@ -1,7 +1,23 @@
+import Client from "~/client/client";
 import type { Route } from "../+types/root";
 
-export async function loader({ params }: Route.LoaderArgs) {}
+export async function clientLoader({ params }: Route.LoaderArgs) {
+  const response = await Client.get(`flags/${params.flagId}`);
+
+  return response.data;
+}
 
 export default function Component({ loaderData }: Route.ComponentProps) {
-  return <div>Flag details</div>;
+  const details: any = loaderData;
+
+  if (!details) {
+    return <div>Error loading details</div>;
+  }
+
+  return (
+    <div>
+      <div>Flag details</div>
+      <div>{details.name}</div>
+    </div>
+  );
 }
