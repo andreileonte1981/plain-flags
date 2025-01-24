77f2bf866d3d37387607b41a123aa2b968e10032
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-01-24 18:38:05 +0200

    Constraint card design

--- dashboard/app/routes/constraints.tsx
+++ dashboard/app/routes/constraints.tsx
@@ -28,8 +28,46 @@ export default function Constraints({ loaderData }: Route.ComponentProps) {
       <div>
         <ul>
           {constraints.map((c) => (
-            <li className="border-b m-2" key={c.id}>
-              <div>{c.description}</div>
+            <li className="m-2" key={c.id}>
+              <div
+                id={`flagcard_${c.id}`}
+                className="border-2 rounded border-gray-300 m-2 p-2 text-gray-500 font-semibold"
+              >
+                <div className="flex justify-between my-2 border-b">
+                  <div className="font-bold text-gray-700">{c.description}</div>
+                  <div className="text-xs text-gray-400">id: {c.id}</div>
+                </div>
+
+                <div>
+                  <div>
+                    <div className="my-2">
+                      For:{" "}
+                      <span className="font-bold text-gray-700">{c.key}</span>
+                    </div>
+
+                    <div>
+                      Named:{" "}
+                      <span className="font-bold text-gray-700">
+                        {c.values.join(", ")}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
+                {c.flags.length > 0 && (
+                  <div className="border-2 border-purple-200 rounded p-2 mt-2">
+                    <div className="m-2">Flags constrained:</div>
+
+                    <ul>
+                      {c.flags.map((f) => (
+                        <li key={f} className="m-2">
+                          {f}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
             </li>
           ))}
         </ul>
