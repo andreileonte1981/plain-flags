4a0abe4b0028049fcbc18aae23527349cbee2a10
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-01-28 13:20:33 +0200

    Reusable expand component

---
+++ dashboard/app/components/reusables/expand.tsx
@@ -0,0 +1,41 @@
+import { useState, type ReactNode } from "react";
+import ExpandIcon from "../icons/expandIcon";
+import CollapseIcon from "../icons/collapseIcon";
+
+export default function Expand(props: { children: ReactNode }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <>
+      {!expanded ? (
+        <div className="flex flex-col items-stretch w-full mt-2 px-2">
+          <button
+            className="flex items-center justify-center gap-1 p-1 w-full border-2 border-gray-500 rounded uppercase text-xs font-extrabold hover:text-black hover:shadow active:scale-y-95"
+            onClick={() => setExpanded(true)}
+          >
+            Show
+            <ExpandIcon />
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col items-stretch w-full mt-2 px-2">
+          <button
+            className="flex items-center justify-center gap-1 p-1 w-full border-2 border-gray-500 rounded uppercase text-xs font-extrabold hover:text-black hover:shadow active:scale-y-95"
+            onClick={() => setExpanded(false)}
+          >
+            Hide
+            <CollapseIcon />
+          </button>
+          {props.children}
+          <button
+            className="flex items-center justify-center gap-1 p-1 w-full border-2 border-gray-500 rounded uppercase text-xs font-extrabold hover:text-black hover:shadow active:scale-y-95"
+            onClick={() => setExpanded(false)}
+          >
+            Hide
+            <CollapseIcon />
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
--- dashboard/app/routes/flags/components/constraintSection.tsx
+++ dashboard/app/routes/flags/components/constraintSection.tsx
@@ -4,6 +4,7 @@ import AppliedConstraint from "./constraintCards/appliedConstraint";
 import { useState } from "react";
 import ExpandIcon from "~/components/icons/expandIcon";
 import CollapseIcon from "~/components/icons/collapseIcon";
+import Expand from "~/components/reusables/expand";
 
 export default function ConstraintSection(props: {
   linkableConstraints: Constraint[];
@@ -17,34 +18,15 @@ export default function ConstraintSection(props: {
       <div className="flex flex-col items-center w-1/2 text-center border-r-2 pr-1">
         <h1>Available constraints</h1>
 
-        {!expanded ? (
-          <div className="flex flex-col items-stretch w-full mt-2 px-2">
-            <button
-              className="flex items-center justify-center gap-1 p-1 w-full border-2 border-gray-500 rounded uppercase text-xs font-extrabold hover:text-black hover:shadow active:scale-y-95"
-              onClick={() => setExpanded(true)}
-            >
-              Show
-              <ExpandIcon />
-            </button>
-          </div>
-        ) : (
-          <div className="flex flex-col items-stretch w-full mt-2 px-2">
-            <button
-              className="flex items-center justify-center gap-1 p-1 w-full border-2 border-gray-500 rounded uppercase text-xs font-extrabold hover:text-black hover:shadow active:scale-y-95"
-              onClick={() => setExpanded(false)}
-            >
-              Hide
-              <CollapseIcon />
-            </button>
-            <ul className="space-y-2 py-2">
-              {props.linkableConstraints.map((c: Constraint) => (
-                <li key={`available_${c.id}`}>
-                  <LinkableConstraint c={c} flagId={props.flagId} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+        <Expand>
+          <ul className="space-y-2 py-2">
+            {props.linkableConstraints.map((c: Constraint) => (
+              <li key={`available_${c.id}`}>
+                <LinkableConstraint c={c} flagId={props.flagId} />
+              </li>
+            ))}
+          </ul>
+        </Expand>
       </div>
 
       <div className="w-1/2 text-center">
