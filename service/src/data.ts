4f14957c61e4cec96c955fb197171d02f62369cb
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-02-07 15:17:32 +0200

    DB file moved outside service for sharing

--- service/src/data.ts
+++ service/src/data.ts
@@ -9,7 +9,7 @@ import Constraint from "./entities/constraint"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
-    database: "plain-flags.sqlite",
+    database: "../data/plain-flags.sqlite",     // TODO: allow users to configure this
     logging: true,
     synchronize: (process.env.NODE_ENV !== "production"),
     namingStrategy: new SnakeNamingStrategy(),
@@ -26,7 +26,7 @@ export const AppDataSource = new DataSource({
 })
 
 export class Data {
-    static async init(log: FastifyBaseLogger){
+    static async init(log: FastifyBaseLogger) {
         await AppDataSource.initialize().catch((err) => {
             log.error(`DB init error`, err)
             throw err
