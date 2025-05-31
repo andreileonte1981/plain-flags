edf49d127d8a14ded8605ce07ecee543bd3595bd
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-05-31 12:01:54 +0300

    Flag state in Python SDK

--- sdk/python/python/plainflags.py
+++ sdk/python/python/plainflags.py
@@ -3,15 +3,30 @@ import aiohttp
 import asyncio
 
 
+class Constraint:
+    def __init__(self, raw_data: dict):
+        self.key = raw_data.get("key", "")
+        self.values = raw_data.get("values", [])
+
+
+class FlagState:
+    def __init__(self, raw_data: dict):
+        self.is_on = raw_data.get("isOn", False)
+        constraints = raw_data.get("constraints", [])
+        self.constraints = [Constraint(c)
+                            for c in constraints] if constraints else []
+
+
 class PlainFlags():
     def __init__(self, service_url: str, api_key: str, timeout_ms: int, poll_interval_ms: int = 0):
-        self.service_url = service_url
-        self.api_key = api_key
-        self.timeout_ms = timeout_ms
-        self.poll_interval_ms = poll_interval_ms
+        self.__service_url = service_url
+        self.__api_key = api_key
+        self.__timeout_ms = timeout_ms
+        self.__poll_interval_ms = poll_interval_ms
+        self.__flag_states = {}
 
     async def init(self):
-        if self.poll_interval_ms <= 0:
+        if self.__poll_interval_ms <= 0:
             await self.update_state()
             return
         await self._start_polling()
@@ -23,14 +38,16 @@ class PlainFlags():
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
-                    f"{self.service_url}/api/sdk",
-                    headers={"x-api-key": self.api_key},
-                    timeout=aiohttp.ClientTimeout(self.timeout_ms / 1000.0)
+                    f"{self.__service_url}/api/sdk",
+                    headers={"x-api-key": self.__api_key},
+                    timeout=aiohttp.ClientTimeout(self.__timeout_ms / 1000.0)
                 ) as response:
                     if response.status == 200:
                         data = await response.json()
 
-                        pprint(data, indent=2)
+                        self.__flag_states = dict((key, FlagState(value))
+                                                  for key, value in data.items())
+
                     else:
                         raise Exception(
                             f"Failed to fetch flags: {response.status}")
@@ -38,14 +55,24 @@ class PlainFlags():
             print(f"Error fetching flags: {e}")
 
     def is_on(self, flag_name: str, default: bool = False) -> bool:
+        if flag_name in self.__flag_states:
+            return self.__flag_states[flag_name].is_on
+        else:
+            print(
+                f"Flag '{flag_name}' not found, returning default: {default}")
         return default
 
 
 # REMOVE
 if __name__ == "__main__":
     pf = PlainFlags(service_url="http://localhost:5001",
-                    api_key="ersafoi89a0ww3n8yrt493ew84yqjwd34y8r09",
+                    api_key="345fg-1r9vu342-0r91uvnm-102394u-v14mj-20951",
                     timeout_ms=20000,
                     poll_interval_ms=0)
 
-    asyncio.run(pf.init())
+    async def f(pf):
+        await pf.init()
+        on = pf.is_on("Flag001", default=False)
+        print(on)
+
+    asyncio.run(f(pf))
