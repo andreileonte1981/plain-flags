8b5317ed20f1fba5274436b3f49a52775aebeef9
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-10-04 19:44:17 +0300

    Manual update test for Dart SDK

--- service-tests/sdks/dart/test/dart_test.dart
+++ service-tests/sdks/dart/test/dart_test.dart
@@ -48,4 +48,47 @@ void main() async {
       expect(featureFlags.isOn(flagName, false, {}), true);
     },
   );
+
+  test(
+    'Turning on a flag will show it as on in the SDK after a manual state update',
+    () async {
+      final client = Client();
+
+      final token = await userToken(client);
+
+      final flagName = saltUniqued('tda-o');
+
+      final createResponse = await client.post('/api/flags', {
+        "name": flagName,
+      }, token);
+
+      expect(createResponse.statusCode, 201);
+
+      final flagId = createResponse.body['id'];
+
+      final featureFlags = PlainFlags(
+        config: PlainFlagsConfig(
+          serviceUrl: DotEnv.get('API_URL_STATES') ?? 'http://localhost:5001',
+          apiKey: DotEnv.get('API_KEY') ?? '',
+          pollInterval: Duration(seconds: 0),
+        ),
+        infoCallback: null,
+        errorCallback: null,
+      );
+
+      await featureFlags.init();
+
+      final turnOnResponse = await client.post('/api/flags/turnon', {
+        "id": flagId,
+      }, token);
+
+      expect(turnOnResponse.statusCode, 200);
+
+      await Future.delayed(Duration(milliseconds: 1200));
+
+      await featureFlags.updateState();
+
+      expect(featureFlags.isOn(flagName, false, {}), true);
+    },
+  );
 }
