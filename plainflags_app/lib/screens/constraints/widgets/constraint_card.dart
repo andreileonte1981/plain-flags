22d2cec109c9cb10123f1e6b375f9a5d4a83bead
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-11-05 15:17:42 +0200

    Constraint card shows values

--- plainflags_app/lib/screens/constraints/widgets/constraint_card.dart
+++ plainflags_app/lib/screens/constraints/widgets/constraint_card.dart
@@ -20,6 +20,8 @@ class _ConstraintCardState extends ConsumerState<ConstraintCard> {
   @override
   Widget build(BuildContext context) {
     final constraint = widget.constraint;
+    bool editingValues = false;
+
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(8.0)),
@@ -28,14 +30,71 @@ class _ConstraintCardState extends ConsumerState<ConstraintCard> {
       color: Color.fromARGB(255, 255, 240, 251),
       child: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.front_hand, color: Color.fromARGB(255, 136, 136, 136)),
-            SizedBox(width: 8),
-            Flexible(child: Text(constraint.description, softWrap: true)),
-            SizedBox(width: 8),
-            IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.front_hand,
+                  color: Color.fromARGB(255, 136, 136, 136),
+                ),
+                SizedBox(width: 8),
+                Flexible(
+                  child: Text(
+                    constraint.description,
+                    softWrap: true,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(width: 8),
+                IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+              ],
+            ),
+            Divider(),
+            Text('For: ${constraint.key}'),
+            Text('Named:'),
+            editingValues
+                ? Row(children: [])
+                : Container(
+                    margin: EdgeInsets.only(right: 8.0, top: 4.0, bottom: 4.0),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 4.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 224, 224, 224),
+                      borderRadius: BorderRadius.circular(2.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            for (var value in constraint.values) Text(value),
+                          ],
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(4.0),
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              color: Color.fromARGB(255, 50, 50, 50),
+                              width: 1.0,
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.edit),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
           ],
         ),
       ),
