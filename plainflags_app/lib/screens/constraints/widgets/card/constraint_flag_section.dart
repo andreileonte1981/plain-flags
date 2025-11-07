670f879c83e99fde4600ee44ad2f7351caa47408
andreileonte1981 <adw_ro@yahoo.co.uk>
2025-11-08 01:15:02 +0200

    Tapping on flag in constraint card shows flag details

--- plainflags_app/lib/screens/constraints/widgets/card/constraint_flag_section.dart
+++ plainflags_app/lib/screens/constraints/widgets/card/constraint_flag_section.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:plainflags_app/domain/constraint.dart';
+import 'package:plainflags_app/providers/current_flag_id.dart';
+import 'package:plainflags_app/screens/flags/flag_details.dart';
 import 'package:plainflags_app/screens/flags/widgets/flag_badge.dart';
 
-class ConstraintFlagSection extends StatelessWidget {
+class ConstraintFlagSection extends ConsumerStatefulWidget {
   final Constraint constraint;
 
   const ConstraintFlagSection({super.key, required this.constraint});
 
+  @override
+  ConsumerState<ConstraintFlagSection> createState() =>
+      _ConstraintFlagSectionState();
+}
+
+class _ConstraintFlagSectionState extends ConsumerState<ConstraintFlagSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,73 +36,89 @@ class ConstraintFlagSection extends StatelessWidget {
             'Flags constrained:',
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          for (var flag in constraint.flags)
+          for (var flag in widget.constraint.flags)
             Padding(
               padding: const EdgeInsets.all(2.0),
-              child: Row(
-                children: [
-                  Flexible(
-                    child: flag.isOn
-                        ? Text(
-                            flag.name,
-                            style: TextStyle(color: Colors.green[800]),
-                            softWrap: true,
-                            overflow: TextOverflow.visible,
-                          )
-                        : Text(
-                            flag.name,
-                            style: TextStyle(color: Colors.grey[800]),
-                            softWrap: true,
-                            overflow: TextOverflow.visible,
-                          ),
-                  ),
-                  SizedBox(width: 8),
-                  flag.isOn
-                      ? FlagBadge(
-                          backgroundColor: const Color.fromARGB(
-                            255,
-                            206,
-                            255,
-                            207,
-                          ),
-                          strokeColor: const Color.fromARGB(255, 134, 179, 135),
-                          child: Row(
-                            children: [
-                              Text(
-                                'on',
-                                style: TextStyle(
-                                  color: Colors.green[800],
-                                  fontWeight: FontWeight.w900,
+              child: TextButton(
+                onPressed: () async {
+                  ref.read(currentFlagIdProvider.notifier).setFlagId(flag.id);
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FlagDetails(flagId: flag.id),
+                    ),
+                  );
+                },
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: flag.isOn
+                          ? Text(
+                              flag.name,
+                              style: TextStyle(color: Colors.green[800]),
+                              softWrap: true,
+                              overflow: TextOverflow.visible,
+                            )
+                          : Text(
+                              flag.name,
+                              style: TextStyle(color: Colors.grey[800]),
+                              softWrap: true,
+                              overflow: TextOverflow.visible,
+                            ),
+                    ),
+                    SizedBox(width: 8),
+                    flag.isOn
+                        ? FlagBadge(
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              206,
+                              255,
+                              207,
+                            ),
+                            strokeColor: const Color.fromARGB(
+                              255,
+                              134,
+                              179,
+                              135,
+                            ),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'on',
+                                  style: TextStyle(
+                                    color: Colors.green[800],
+                                    fontWeight: FontWeight.w900,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(width: 4),
-                              Icon(
-                                Icons.flag,
-                                color: Colors.green[800],
-                                size: 16,
-                              ),
-                            ],
-                          ),
-                        )
-                      : FlagBadge(
-                          backgroundColor: Colors.grey[400]!,
-                          strokeColor: Colors.grey[600]!,
-                          child: Row(
-                            children: [
-                              Text('off'),
-                              SizedBox(width: 4),
-                              Transform.rotate(
-                                angle: 1.2,
-                                child: Icon(
-                                  Icons.flag_outlined,
-                                  color: Colors.grey[800],
+                                SizedBox(width: 4),
+                                Icon(
+                                  Icons.flag,
+                                  color: Colors.green[800],
                                   size: 16,
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
+                          )
+                        : FlagBadge(
+                            backgroundColor: Colors.grey[400]!,
+                            strokeColor: Colors.grey[600]!,
+                            child: Row(
+                              children: [
+                                Text('off'),
+                                SizedBox(width: 4),
+                                Transform.rotate(
+                                  angle: 1.2,
+                                  child: Icon(
+                                    Icons.flag_outlined,
+                                    color: Colors.grey[800],
+                                    size: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                ],
+                  ],
+                ),
               ),
             ),
         ],
